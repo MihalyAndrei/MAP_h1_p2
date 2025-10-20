267ac3851e2bc463134bd04da4911002a3123865
Mihaly Cozmuta Andrei <andrei.mihaly@stud.ubbcluj.ro>
2025-10-20 11:23:28 +0300

    added first function and a main for output checking

--- src/mmFunctions.java
+++ src/mmFunctions.java
@@ -1,2 +1,12 @@
 public class mmFunctions {
+    public static int getMaxNumber(int[] numbers) {
+        int max=numbers[0];
+        for(int number:numbers){
+            if(max<number){
+                max=number;
+            }
+        }
+        return max;
+    }
 }
+
